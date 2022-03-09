bc8ffd764859df63f093445524a9787d7a028915
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-09 08:12:44 +0100

    Created pokemonList variable which contains the Pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert("Hello world");
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+// This array contains the Pokemon data for the app
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['monster', 'grass'],
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: ['monster', 'dragon'],
+  },
+  {
+    name: 'Pikachu',
+    height: 4,
+    types: ['field', 'fairy'],
+  },
+];
