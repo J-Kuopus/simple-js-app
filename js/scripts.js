6d6a2681e009b0e64a1231dc60928d4437b0b7e3
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-16 12:47:01 +0100

    Wrapped pokemonList array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,51 @@
 // These arrays contain the Pokemon data for the app
-let pokemonList = [
-  {
-    name: 'Wartortle',
-    height: 1.0,
-    types: ['monster', 'water-1'],
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['monster', 'dragon'],
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    types: ['field', 'fairy'],
-  },
-  {
-    name: 'Clefable',
-    height: 1.3,
-    types: ['fairy'],
-  },
-  {
-    name: 'Pidgey',
-    height: 0.3,
-    types: ['flying'],
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['monster', 'water1'],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Wartortle',
+      height: 1.0,
+      types: ['monster', 'water-1'],
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ['monster', 'dragon'],
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      types: ['field', 'fairy'],
+    },
+    {
+      name: 'Clefable',
+      height: 1.3,
+      types: ['fairy'],
+    },
+    {
+      name: 'Pidgey',
+      height: 0.3,
+      types: ['flying'],
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['monster', 'water1'],
+    },
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
 pokemonList.forEach(function (pokemon) {
   let result = pokemon.height >= 1 ? " - Wow, that's big!" : '';
