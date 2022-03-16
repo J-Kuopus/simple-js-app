a1b716e82fa93734c11f266e763817328e1b3e83
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-16 11:30:57 +0100

    Removed pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,36 @@
 // These arrays contain the Pokemon data for the app
-let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Wartortle',
-      height: 1.0,
-      types: ['monster', 'water-1'],
-    },
-    {
-      name: 'Charmander',
-      height: 0.6,
-      types: ['monster', 'dragon'],
-    },
-    {
-      name: 'Pikachu',
-      height: 0.4,
-      types: ['field', 'fairy'],
-    },
-    {
-      name: 'Clefable',
-      height: 1.3,
-      types: ['fairy'],
-    },
-    {
-      name: 'Pidgey',
-      height: 0.3,
-      types: ['flying'],
-    },
-    {
-      name: 'Squirtle',
-      height: 0.5,
-      types: ['monster', 'water1'],
-    },
-  ];
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  return {
-    getAll: getAll,
-    add: add,
-  };
-})();
-
-pokemonList.forEach(function (name) {
-  document.write(name);
-});
+let pokemonList = [
+  {
+    name: 'Wartortle',
+    height: 1.0,
+    types: ['monster', 'water-1'],
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['monster', 'dragon'],
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['field', 'fairy'],
+  },
+  {
+    name: 'Clefable',
+    height: 1.3,
+    types: ['fairy'],
+  },
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    types: ['flying'],
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['monster', 'water1'],
+  },
+];
 
 //This lists the Pokemon in the DOM and marks them if they are size 1 or larger
 /* document.write('<ul>');
