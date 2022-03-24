89218b60094cb22b0c81e848c7a5a6c66a989366
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-24 11:17:46 +0100

    Changed text color

--- js/scripts.js
+++ js/scripts.js
@@ -76,10 +76,10 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showModal(name, height, type, imageUrl) {
+  function showModal(name, height, types, imageUrl) {
     let modalContainer = document.querySelector('#modal-container');
     document.querySelector('.modal-title').innerText = name;
-    let details = 'Height: ' + height + '<br>' + 'Type: ' + type;
+    let details = 'Height: ' + height + '<br>' + 'Type: ' + types;
 
     document.querySelector('.modal-text').innerHTML = details;
     document.querySelector('.modal-image').setAttribute('src', imageUrl);
