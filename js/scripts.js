de4d00a1129b98ca87743418cae0c4267dbc9657
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-24 09:39:25 +0100

    Updated showModal function and parameters

--- js/scripts.js
+++ js/scripts.js
@@ -85,9 +85,7 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showModal(name, height, type, imageUrl) {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.add('is-visible');
+  function showModal(title, text) {
     let modal = document.createElement('div');
     modal.classList.add('modal');
     modalContainer.appendChild(modal);
