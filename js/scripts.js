88c63e83658aa63181f732b3de01111f166ffe8f
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-28 08:32:51 +0200

    Called showLoadingMessage function

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,7 @@ let pokemonRepository = (function () {
   }
 
   function loadDetails(item) {
+    showLoadingMessage();
     let url = item.detailsUrl;
     return fetch(url)
       .then(function (response) {
