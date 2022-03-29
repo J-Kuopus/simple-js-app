8d40e138faf310e9a419674383a54f18e18d3f67
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-03-29 08:42:32 +0200

    Added group-list-item class to li elements

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ let pokemonRepository = (function () {
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('#pokemon-list');
     let listItem = document.createElement('li');
+    listItem.classList.add('group-list-item');
     let button = document.createElement('button');
     button.addEventListener('click', function () {
       showDetails(pokemon);
