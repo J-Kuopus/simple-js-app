f92d911e0874b86b40b9ebb5843a59e0f2c9ab17
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-04-01 08:44:39 +0200

    Added function comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  // Function for adding new pokemon to the list
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
@@ -15,46 +16,55 @@ let pokemonRepository = (function () {
     }
   }
 
+  // Returns all the pokemon in the list
   function getAll() {
     return pokemonList;
   }
-
+  // Creates li tags for pokemon items, adds them to ul in HTML
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('#pokemon-list');
     let listItem = document.createElement('li');
     listItem.classList.add('group-list-item');
+
+    // Creates button for each pokemon item
     let button = document.createElement('button');
     button.classList.add('btn-primary');
     button.setAttribute('data-toggle', 'modal');
     button.setAttribute('data-target', '#pokemonModal');
+
+    // Activates showDetails function when user clicks pokemon button
     button.addEventListener('click', function () {
       showDetails(pokemon);
     });
+    // Add pokemon name to button
     button.innerText = pokemon.name;
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
   }
-
+  // Fetches API data then returns JSON data as list
   function loadList() {
-    return fetch(apiUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url,
-          };
-          add(pokemon);
-        });
-      })
-      .catch(function (e) {
-        console.error(e);
-      });
+    return (
+      fetch(apiUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        // Iterates over JSON data and defines pokemon variable
+        .then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url,
+            };
+            add(pokemon);
+          });
+        })
+        .catch(function (e) {
+          console.error(e);
+        })
+    );
   }
-
+  // Takes JSON data and organizes it
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url)
@@ -70,20 +80,25 @@ let pokemonRepository = (function () {
         console.error(e);
       });
   }
-  //This adds the pokemon details into the modal
+  // Loads the pokemon details into the modal
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
     });
   }
-  // This makes modal appear when user clicks Pokemon button
+  // Makes modal appear when user clicks Pokemon button
   function showModal(name, height, imageUrl) {
+    // Adds pokemon name as modal title
     document.querySelector('.modal-title').innerText = name;
+
+    // Adds pokemon image into body of modal
     document.querySelector('.modal-image').setAttribute('src', imageUrl);
+
+    // Adds pokemon height to body of modal
     let details = 'Height: ' + height;
     document.querySelector('.modal-text').innerHTML = details;
   }
-
+  // Return keys that allow you to access the IIFE functions
   return {
     add: add,
     getAll: getAll,
@@ -94,7 +109,7 @@ let pokemonRepository = (function () {
   };
 })();
 
-// Loop that lists all the Pokemon
+// Loop that iterates over the Pokemon list
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
