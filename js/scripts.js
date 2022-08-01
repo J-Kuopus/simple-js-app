0c9a7f6205ba60896af1ddf0d6e114b809cb6a98
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-08-01 11:56:31 +0200

    Updated comments for jsdoc

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
-// IIFE for pokemonList array
+/**
+ * IIFE that includes all functions for Pokedex Repository
+ * @namespace pokemonRepository
+ */
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  // Adds new pokemon to the list and validates the input
+  /**
+   * Adds new pokemon to the list and validates the input
+   * @param {object} pokemon - name and details of pokemon
+   * @memberof pokemonRepository
+   */
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
@@ -16,35 +23,64 @@ let pokemonRepository = (function () {
     }
   }
 
-  // Returns all the pokemon in the list
+  /**
+   * Gets a list of all pokemon
+   * @returns {array} - array of pokemon objects in JSON format
+   * @memberof pokemonRepository
+   */
   function getAll() {
     return pokemonList;
   }
-  // Creates li tags for pokemon items, adds them to ul in HTML
+
+  /**
+   * Creates li tags for pokemon items, adds them to ul in "index.html"
+   * @param {string} pokemon - name of pokemon
+   * @memberof pokemonRepository
+   */
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('#pokemon-list');
     let listItem = document.createElement('li');
     listItem.classList.add('group-list-item');
 
-    // Creates button for each pokemon item
+    /**
+     * Creates button element for each pokemon item
+     * @function createElement
+     * @memberof pokemonRepository
+     */
     let button = document.createElement('button');
     button.classList.add('btn-primary');
     button.setAttribute('data-toggle', 'modal');
     button.setAttribute('data-target', '#pokemonModal');
 
-    // Activates showDetails function when user clicks pokemon button
+    /**
+     * Adds event listener to pokemon buttons and activates showDetails function on click
+     * @function addEventListener
+     * @fires showDetails
+     * @event click
+     * @memberof pokemonRepository
+     */
     button.addEventListener('click', function () {
       showDetails(pokemon);
     });
 
-    // Adds pokemon name to button
+    /**
+     * Adds pokemon name to button element
+     */
     button.innerText = pokemon.name;
     button.classList.add('pokemon-button');
+
+    /**
+     * Appends button elements to "index.html"
+     */
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
   }
 
-  // Fetches API data then returns JSON data as list
+  /**
+   * Fetches API data then returns JSON data as list
+   * @returns {object} pokemon data - pokemon name and details
+   * @memberof pokemonRepository
+   */
   function loadList() {
     return (
       fetch(apiUrl)
@@ -66,7 +102,12 @@ let pokemonRepository = (function () {
         })
     );
   }
-  // Takes JSON data and organizes it
+
+  /**
+   * Loads the JSON data details about each Pokemon
+   * @param {string} item - name of pokemon
+   * @returns {object} details- image, height, type traits of pokemon
+   */
   function loadDetails(item) {
     let url = item.detailsUrl;
     return (
@@ -86,13 +127,25 @@ let pokemonRepository = (function () {
         })
     );
   }
-  // Loads the pokemon details into the modal
+
+  /**
+   * Loads the pokemon details into the modal
+   * @param {object} pokemon - name and details of pokemon
+   * @memberof pokemonRepository
+   */
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
     });
   }
-  // Makes modal appear when user clicks Pokemon button
+
+  /**
+   * Opens modal when user clicks pokemon button and displays details
+   * @param {string} name - name of pokemon
+   * @param {string} height - height value of pokemon
+   * @param {string} imageUrl - image url of pokemon
+   * @memberof pokemonRepository
+   */
   function showModal(name, height, imageUrl) {
     // Adds pokemon name as modal title
     document.querySelector('.modal-title').innerText = name;
